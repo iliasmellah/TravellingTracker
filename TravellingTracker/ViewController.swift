2e7115ffae58355652b9099c5ae1eab7384d2861
Ilias <ilias.mellah@gmail.com>
2019-07-09 09:34:23 +0800

    Trip deleting working

--- TravellingTracker/ViewController.swift
+++ TravellingTracker/ViewController.swift
@@ -11,17 +11,15 @@ import CoreData
 
 class ViewController: UIViewController,UITableViewDataSource, UITableViewDelegate {
     
-    //var tripNames : [String] = ["Bali", "Maroc", "Espagne"]
+    //Collcetion of Trips to be displayed in self.tripsTable
     var trips : [Trip] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
-            self.alertError(errorMsg: "Could not save the trip", userInfo: "unknown reason")
-            return
-        }
-        let context = appDelegate.persistentContainer.viewContext
+        
+        //first get context of persistent data
+        guard let context = self.getContext(errorMsg: "Could note load data") else {return}
         
         //creates a request for "Trip" entity
         let request : NSFetchRequest<Trip> = Trip.fetchRequest()
@@ -29,14 +27,17 @@ class ViewController: UIViewController,UITableViewDataSource, UITableViewDelegat
             try self.trips = context.fetch(request)
         }
         catch let error as NSError{
-            self.alertError(errorMsg: "\(error)", userInfo: "\(error.userInfo)")
+            self.alert(error: error)
         }
         
     }
     
+    /// TableView controlled by self that displays collection of Trips
     @IBOutlet weak var tripsTable: UITableView!
     
-    //Création d'un nouveau trip dans la table view cell
+    /// Displays adialog box to allow user to enter a trip name. Then creates of a new Trip, add it to table view and saves data
+    ///
+    /// - Parameter sender: object that trigger action
     @IBAction func createAction(_ sender: Any) {
         let alert = UIAlertController(title: "New Trip", message: "Create a new trip", preferredStyle: .alert)
         let saveAction = UIAlertAction(title: "Create", style: .default)
@@ -55,14 +56,14 @@ class ViewController: UIViewController,UITableViewDataSource, UITableViewDelegat
         present(alert, animated: true)
     }
     
+    // MARK: - Trip data managment -
 
+    /// create a new Trip, add it to the collection and saves it
+    ///
+    /// - Parameter name: name of Trip to be added
     func saveNewTrip(withName name: String) {
         //first get context into application delegate
-        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
-            self.alertError(errorMsg: "Could not save the trip", userInfo: "unknown reason")
-            return
-        }
-        let context = appDelegate.persistentContainer.viewContext
+        guard let context = self.getContext(errorMsg: "Save failed") else { return }
         
         //creates a Trips managedObject
         let trip = Trip(context: context)
@@ -73,18 +74,32 @@ class ViewController: UIViewController,UITableViewDataSource, UITableViewDelegat
             try context.save()
             self.trips.append(trip)
         } catch let error as NSError {
-            self.alertError(errorMsg: "\(error)", userInfo: "\(error.userInfo)")
+            self.alert(error: error)
             return
         }
     }
     
-    func alertError(errorMsg error : String, userInfo user: String = ""){
-        let alert = UIAlertController(title: error, message: user, preferredStyle: .alert)
-        let cancelAction = UIAlertAction(title: "Ok", style: .default)
-        alert.addAction(cancelAction)
-        present(alert, animated: true)    }
+    //suppression d'un trip
+    /// deletes a trip fro mcollection thanks to its index
+    /// - Precondition: tripWithIndex must be into bound of colelction
+    /// - Parameter index: tripWithIndex description
+    /// - Returns: true if collection occurs, else false
+    func delete(tripWithIndex index: Int) -> Bool {
+        guard let context = self.getContext(errorMsg: "Could not delete Trip") else {return false}
+        let trip = self.trips[index]
+        context.delete(trip)
+        do{
+            try context.save()
+            self.trips.remove(at: index)
+            return true
+        }
+        catch let error as NSError {
+            self.alert(error: error)
+            return false
+        }
+    }
  
-    
+    // MARK: - TableView data source protocol -
     // Return the number of rows for the table.
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.trips.count
@@ -98,34 +113,7 @@ class ViewController: UIViewController,UITableViewDataSource, UITableViewDelegat
         return cell
     }
     
-    //suppression d'un trip
-    /*
-     func delete(tripWithIndex index: Int) -> Bool {
-     guard let context = self.getContext(errorMsg: "Could not delete Trip")
-     else {return false}
-     let trip = self.tripNames[index]
-     context.delete(trip)
-     do{
-     try context.save()
-     self.tripNames.remove(at: index)
-     return true
-     }
-     catch let error as NSError {
-     self.alert(error: error)
-     return false
-     }
-     }
-     
-     func getContext(errorMsg: String, userInfoMsg: String = "could not retrieve data context") -> NSManagedObjectContext?{
-     guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
-     self.alert(WithTitle: errorMsg, andMessage: userInfoMsg)
-     return nil
-     }
-     return appDelegate.persistentContainer.viewContext
-     }
-     
-     
-     //tell if a particular row can be edited
+    //tell if a particular row can be edited
     func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         return true
     }
@@ -136,11 +124,46 @@ class ViewController: UIViewController,UITableViewDataSource, UITableViewDelegat
         if (editingStyle == UITableViewCell.EditingStyle.delete) {
             self.tripsTable.beginUpdates()
             if self.delete(tripWithIndex: indexPath.row){
-                self.tripsTable.deleteRows(at: [indexPath], with: <#T##UITableView.RowAnimation#>)
+                self.tripsTable.deleteRows(at: [indexPath], with: .automatic)
             }
             self.tripsTable.endUpdates()
         }
     }
-*/
+    
+    
+    // MARK: - helper methods -
+    /// get context of core data initialized in application delegate
+    ///
+    /// - Parameters:
+    ///   - errorMsg: main error message
+    ///   - userInfoMsg: additional information user wants to display
+    /// - Returns: context of CoreData
+    func getContext(errorMsg: String, userInfoMsg: String = "could not retrieve data context") -> NSManagedObjectContext?{
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            self.alert(WithTitle: errorMsg, andMessage: userInfoMsg)
+            return nil
+        }
+        return appDelegate.persistentContainer.viewContext
+    }
+    
+    /// Show an alert dialog box with 2 messages
+    ///
+    /// - Parameters:
+    ///   - title: title of dialog box seen as main message
+    ///   - msg: additional message used to describe context of additional information
+    func alert(WithTitle title : String, andMessage msg : String = "") {
+        let alert = UIAlertController(title: title, message: msg, preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "Ok", style: .default)
+        alert.addAction(cancelAction)
+        present(alert, animated: true)
+    }
+    
+    /// Show an alert to inform about an error
+    ///
+    /// - Parameter error: error we want information about
+    func alert(error: NSError) {
+        self.alert(WithTitle: "\(error)", andMessage: "\(error.userInfo)")
+    }
+
 }
 
