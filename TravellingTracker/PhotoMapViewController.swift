634bd28705dd094956db8e787f834b5ace108fbe
Ilias <ilias.mellah@gmail.com>
2019-07-21 19:43:11 +0800

    Map Tests

--- TravellingTracker/PhotoMapViewController.swift
+++ TravellingTracker/PhotoMapViewController.swift
@@ -8,10 +8,13 @@
 
 import UIKit
 import Photos
+import AssetsLibrary
+import MapKit
 
 class PhotoMapViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var mapView: MKMapView!
     @IBAction func importImage(_ sender: Any) {
         /*
         let image = UIImagePickerController()
@@ -48,27 +51,66 @@ class PhotoMapViewController: UIViewController, UINavigationControllerDelegate, 
         }))
         
         
-        self.present(alert, animated: true, completion: nil)    }
-    
+        self.present(alert, animated: true, completion: nil)
+    }
+   
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
         imageView.image = image
         
+        if let URL = info[UIImagePickerController.InfoKey.referenceURL] as? URL {
+            let opts = PHFetchOptions()
+            opts.fetchLimit = 1
+            let assets = PHAsset.fetchAssets(withALAssetURLs: [URL], options: opts)
+            let asset = assets[0]
+            print("\n Latitude : \n", asset.location?.coordinate.latitude as Any)
+            print("\n Longitude : \n", asset.location?.coordinate.longitude as Any)
+        }
+        
         picker.dismiss(animated: true, completion: nil)
     }
     
+
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         picker.dismiss(animated: true, completion: nil)
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        configureLocationServices()
         // Do any additional setup after loading the view.
     }
     
-
+    // - MARK : Map Tests -
+    private let locationManager = CLLocationManager()
+    private var currentCoordinate : CLLocationCoordinate2D?
+    
+    private func configureLocationServices() {
+        locationManager.delegate = self
+        
+        
+        let status = CLLocationManager.authorizationStatus()
+        
+        if status == .notDetermined {
+            locationManager.requestAlwaysAuthorization()
+        } else if (status == .authorizedAlways || status == .authorizedWhenInUse) {
+            beginLocationUpdates(locationManager: locationManager)
+        }
+    }
+    
+    private func beginLocationUpdates(locationManager: CLLocationManager) {
+        mapView.showsUserLocation = true
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+    }
+    
+    private func zoomToLatestLocation(with coordinate: CLLocationCoordinate2D) {
+        let zoomRegion = MKCoordinateRegion(center: coordinate, latitudinalMeters: 10000, longitudinalMeters: 10000)
+        mapView.setRegion(zoomRegion, animated: true)
+    }
+    
     /*
     // MARK: - Navigation
 
@@ -79,4 +121,42 @@ class PhotoMapViewController: UIViewController, UINavigationControllerDelegate, 
     }
     */
 
+}
+
+extension PhotoMapViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print("Location reçue")
+        
+        // - MARK : Localiser polytech à l'initialisation -
+        let latitudeString : String = "43.63277"
+        let longitudeString : String = "3.8626449"
+        
+        let latitudeDegrees : CLLocationDegrees = Double(latitudeString) as! CLLocationDegrees
+        let longitudeDegrees : CLLocationDegrees = Double(longitudeString) as! CLLocationDegrees
+        
+        let locationPolytech : CLLocation = CLLocation.init(latitude: latitudeDegrees, longitude: longitudeDegrees)
+       
+        let center = CLLocationCoordinate2D(latitude: locationPolytech.coordinate.latitude, longitude: locationPolytech.coordinate.longitude)
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        mapView.setRegion(region, animated: true)
+        
+        /*
+         guard let latestLocation = locations.first else {return}
+
+        if currentCoordinate == nil {
+            zoomToLatestLocation(with: latestLocation.coordinate)
+        }
+        
+        currentCoordinate = latestLocation.coordinate
+ */
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        print("Statut changé : ", status)
+        if (status == .authorizedAlways || status == .authorizedWhenInUse) {
+            beginLocationUpdates(locationManager: manager)
+        } else {
+            print("pas le droit")
+        }
+    }
 }
